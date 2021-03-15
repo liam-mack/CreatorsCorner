a3b69b79823d678c52b12b5acb4ace7b2d9112dd
liam-mack <liammma9@gmail.com>
2021-03-14 22:20:14 -0400

    submit button fix

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -39,7 +39,7 @@ function Contact() {
               required
             ></textarea>
             <h6 className="contactEmail"><i className="fa fa-envelope-o" aria-hidden="true"></i> creatorscornercu@gmail.com</h6>
-            <input className="btn pinkBtn" type="submit" value="Submit" />
+            <input className="pinkBtn" type="submit" value="Submit" />
           </form>
         </div>
       </div>
